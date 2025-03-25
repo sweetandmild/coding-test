6d75f4fe31e6b884f1347f64c636f7f9d4862ab0
jeongtaesu <taesoo502@gmail.com>
2025-03-25 15:24:15 +0900

    docs(main) : create repository

---
+++ src/data_structure/array/BasicArraySort.java
@@ -0,0 +1,13 @@
+package data_structure.array;
+
+import java.util.Arrays;
+
+public class BasicArraySort {
+   
+    public static int[] solution(int[] arr){
+
+        Arrays.sort(arr);
+
+        return arr;
+    }
+}
---
+++ src/data_structure/array/ProblemRunMain.java
@@ -0,0 +1,16 @@
+package data_structure.array;
+
+import java.util.Arrays;
+
+public class ProblemRunMain {
+    public static void main(String[] args) {
+        int[] arr1 = {1,-5,2,4,3};
+        int[] arr2 = {2,1,1,3,2,5,4};
+        int[] arr3 = {6,1,7};
+
+
+        System.out.println( Arrays.toString(BasicArraySort.solution(arr1)));
+        System.out.println( Arrays.toString(BasicArraySort.solution(arr2)));
+        System.out.println( Arrays.toString(BasicArraySort.solution(arr3)));
+    }
+}
